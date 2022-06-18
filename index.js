5aca85de3dd8681a339858d7353d3db9743bd36f
thaisealves <alves.thaisesilva@gmail.com>
2022-06-18 02:03:01 -0300

    Sign-up and tweets POST finalized

--- index.js
+++ index.js
@@ -2,7 +2,22 @@ import express from "express";
 import cors from "cors";
 
 const app = express();
-
 app.use(cors());
+app.use(express.json());
+let avatar = "";
+const users = [];
+const tweets = [];
+app.post("/sign-up", (req, res) => {
+  avatar = req.body.avatar;
+  users.push({ username: req.body.username, avatar: avatar });
+  console.log(users);
+  res.send("OK");
+});
+
+app.post("/tweets", (req, res) => {
+  tweets.unshift({ username: req.body.username, tweet: req.body.tweet });
+  res.send("OK");
+  console.log(tweets);
+});
 
 app.listen(5000);
