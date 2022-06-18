9b57b93e1d39575b0b61e2edbef2083d20f128c2
thaisealves <alves.thaisesilva@gmail.com>
2022-06-18 02:37:32 -0300

    GET from tweets done

--- index.js
+++ index.js
@@ -5,19 +5,28 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 let avatar = "";
+let username = "";
 const users = [];
 const tweets = [];
 app.post("/sign-up", (req, res) => {
   avatar = req.body.avatar;
-  users.push({ username: req.body.username, avatar: avatar });
+  username = req.body.username;
+  users.push({ username, avatar });
   console.log(users);
   res.send("OK");
 });
 
 app.post("/tweets", (req, res) => {
-  tweets.unshift({ username: req.body.username, tweet: req.body.tweet });
+  username = req.body.username;
+  const sameUser = users.find((el) => el.username === username);
+  tweets.unshift({ username, avatar: sameUser.avatar, tweet: req.body.tweet });
   res.send("OK");
   console.log(tweets);
+});
+
+app.get("/tweets", (req, res) => {
+  const MAX_SHOWN_TWEETS = 10;
+  res.send(tweets.slice(0, MAX_SHOWN_TWEETS));
 });
 
 app.listen(5000);
